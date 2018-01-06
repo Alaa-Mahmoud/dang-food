9dd014020f2750343dbe24d9e260a41ee6b204e8
Alaa-Mahmoud <alaamahmoud.fci.scu@gmail.com>
2018-01-06 21:25:54 +0200

    finish tag page

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,20 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(StoreController.getStores));
 
 router.get('/stores', catchErrors(StoreController.getStores));
-
 router.get('/add', StoreController.add);
-
 router.post('/add', StoreController.upload,
     catchErrors(StoreController.resize), catchErrors(StoreController.createStore)
 );
-
 router.post('/add/:id', StoreController.upload,
     catchErrors(StoreController.resize), catchErrors(StoreController.updateStore));
 
 router.get('/stores/:id/edit', catchErrors(StoreController.editStore));
 
 router.get('/store/:slug', catchErrors(StoreController.getStoreBySlug))
+
+router.get('/tags', catchErrors(StoreController.getStoresByTag));
+
+router.get('/tags/:tag', catchErrors(StoreController.getStoresByTag));
+
+
 module.exports = router;
