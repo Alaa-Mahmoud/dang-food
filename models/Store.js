3e76fdf5fefc4721726bebc5ce81eb0911e81d32
Alaa-Mahmoud <alaamahmoud.fci.scu@gmail.com>
2018-01-06 15:14:32 +0200

    suppor adding a store using google geolocation

--- models/Store.js
+++ models/Store.js
@@ -13,7 +13,25 @@ const storeSchema = new mongoose.Schema({
         type: String,
         trim: true,
     },
-    tags: [String]
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: "You must supply us with coordinates",
+        }],
+        address: {
+            type: String,
+            required: "You must supply us with address",
+        },
+    }
 });
 
 /********************************
